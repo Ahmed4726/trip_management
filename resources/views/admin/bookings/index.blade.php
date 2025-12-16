b5f970d63f19448c2e1b9a6f6eb742754d656729
uzair <artisan.ilyas@gmail.com>
2025-12-16 23:59:03 +0500

    Update index.blade.php

--- resources/views/admin/bookings/index.blade.php
+++ resources/views/admin/bookings/index.blade.php
@@ -3,9 +3,9 @@
 <div class="content-wrapper">
     <div class="container pt-3">
         <div class="d-flex justify-content-between align-items-center mb-4">
-            <h2 class="h4">Manage Availabilities</h2>
+            <h2 class="h4">Manage Booking</h2>
             @can('create-trip')
-            <a href="{{ route('trips.create') }}" class="btn btn-primary">Create</a>
+            <a href="{{ route('bookings.create') }}" class="btn btn-primary">Create</a>
             @endcan
         </div>
 
